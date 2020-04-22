e0283d42a39eebaeb96a3b93269ab439a9d90d77
Marivel Quiroz <mquiroz@signal.co>
2020-04-22 13:37:22 -0500

    Modified 70

--- pipeline.groovy
+++ pipeline.groovy
@@ -46,7 +46,7 @@ timeout(240) {
                                 'threads': "${THREADS}",
                                 'rampUp': "${RAMPUP}",
                                 'loopCount': "${LOOPCOUNT}",
-                                'url': "${URL}"
+                                'url': "${ENV}"
                         ]
                         performTest('dummy_test.jmx',"${STAGE_NAME}",setPlanProperties(propertiesMap))
                     }
